72c05d50f596a85ed4424bbf52733e8f6c154f8d
callaing0 <0hmong0212@gmail.com>
2026-01-05 22:08:10 +0900

    Update Test2.java

--- Test2.java
+++ Test2.java
@@ -5,29 +5,31 @@ import java.util.Scanner;
 public class Test2 {
 	public static void main(String[] args) {
 		
-		
-		// 과제 2
-		// 내가 제시한 숫자를 컴퓨터가 맞추게 하기
-		// UP, DOWN 힌트를 줄 것
-		
 		Scanner sc = new Scanner(System.in);
-		
-		int num = 0;
-		int com = (int)Math.floor(Math.random()*50)+1;
-		
-		System.out.print("숫자를 입력하시오. (1 ~ 10) : ");
-		int num = sc.nextInt();
-			
-			while(num == com ){
-				System.out.println("정답입니다!");	
-				break;
-				
-				if(num > com) {
-					System.out.println(+num+"보다 "+com+"의 숫자가 작은가요?");
-				}
-			
-			
-		} 
-		
+
+	        // 내가 제시한 숫자
+	        System.out.print("정답 숫자를 입력하세요 (1 ~ 50): ");
+	        int num = sc.nextInt();
+	        
+	        int min = 0;
+	        int max = 0;
+
+	        while (true) {
+	            int com = (int)(Math.random() * 50) + 1;
+	            
+	            System.out.println("컴퓨터가 추측한 숫자는 " +com+ "입니다.");
+	            System.out.print("힌트로 UP / DOWN / 정답을 입력하세요 : ");
+	            String hint = sc.next();
+
+	            if (hint.equals("UP")) {
+	                min = com + 1;
+	            } else if (hint.equals("DOWN")) {
+	                max = com - 1;
+	            } else if (hint.equals("정답")) {
+	                System.out.println("정답입니다!");
+	                break;
+	            }
+	        }
+	    }
 	}
-}
+
